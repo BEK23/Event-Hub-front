7415fed1ab6414eb4cf3ecc6b65b11236eadbda7
_BEK <62075760+BEK23@users.noreply.github.com>
2025-01-25 18:27:40 +0500

    feat: vertical scoll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import EmblaCarousel from "@/components/embla-carousel";
 import { EmblaOptionsType } from "embla-carousel";
+import { HouseIcon, SearchIcon, UserRoundIcon } from "lucide-react";
 
 const OPTIONS: EmblaOptionsType = {
   axis: "y",
@@ -11,8 +12,14 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col h-dvh">
       <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+
+      <div className="flex items-center justify-around bg-black py-2.5">
+        <SearchIcon className="size-7 text-white" />
+        <HouseIcon className="size-7 text-primary" />
+        <UserRoundIcon className="size-7 text-white" />
+      </div>
     </div>
   );
 }
