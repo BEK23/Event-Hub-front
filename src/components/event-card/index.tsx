407b6389f00f95750375809ab148538eb3ba184b
_BEK <62075760+BEK23@users.noreply.github.com>
2025-01-26 04:17:34 +0500

    fix: image

--- src/components/event-card/index.tsx
+++ src/components/event-card/index.tsx
@@ -15,14 +15,25 @@ export const EventCard: FC<IEvent> = (props) => {
   } = props;
 
   return (
-    <div className="flex flex-col h-full select-none embla__slide__content">
+    <div className="flex flex-col h-full select-none embla__slide__content overflow-auto">
       <div className="relative">
         <Image
           src={`http://localhost:1337${image.url}`}
           alt={title}
+          isBlurred
           classNames={{
-            wrapper: "aspect-video",
-            img: "rounded-none",
+            wrapper: "h-[300px] w-full mx-auto z-20",
+            img: "rounded-none object-cover h-full",
+          }}
+        />
+        <Image
+          src={`http://localhost:1337${image.url}`}
+          alt={title}
+          isBlurred
+          classNames={{
+            wrapper:
+              "h-[300px] w-full mx-auto absolute top-0 overflow-hidden blur-md",
+            img: "rounded-none object-cover",
           }}
         />
 
@@ -35,7 +46,7 @@ export const EventCard: FC<IEvent> = (props) => {
         </Chip>
       </div>
 
-      <div className="px-4">
+      <div className="px-4 mb-4">
         <h1 className="text-2xl font-semibold mt-6">{title}</h1>
         <h2 className="mb-2 text-gray-600">{community?.name}</h2>
 
